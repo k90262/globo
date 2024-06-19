d866836861799de0f4b4b1d7c77ced624bffbbe5
Bill Ho <ycho.bill.lab@gmail.com>
2024-06-19 23:49:12 +0800

    API: Creating a Database
    Command Memo:
     9989  cd Api
     0012  dotnet add package Microsoft.EntityFrameworkCore.Sqlite
    10016* code SeedData.cs
    10017  dotnet add package Microsoft.EntityFrameworkCore.Design
    10019  dotnet ef migrations add initial
    10020  dotnet ef database update

---
+++ Api/Data/HouseDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
+public class HouseDbContext : DbContext 
+{
+    public DbSet<HouseEntity> Hourses => Set<HouseEntity>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData; // About this path on MacOSX, see https://github.com/dotnet/runtime/issues/40353
+        var path = Environment.GetFolderPath(folder); // "/Users/apple/Library/Application Support/"
+        optionsBuilder
+            .UseSqlite($"Data Source={Path.Join(path, "globo_houses.db")}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        SeedData.Seed(builder);
+    }
+}
