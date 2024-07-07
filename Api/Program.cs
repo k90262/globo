b073d490b092208faa61277789beedc6762574ac
Bill Ho <ycho.bill.lab@gmail.com>
2024-07-07 19:46:46 +0800

    [Adding Frontend Routing and Navigation] API: Details Endpoint

--- Api/Program.cs
+++ Api/Program.cs
@@ -21,6 +21,15 @@ app.UseCors(p => p.WithOrigins("http://localhost:3000")
     .AllowAnyHeader().AllowAnyMethod());
 app.UseHttpsRedirection();
 
-app.MapGet("/houses", (IHouseRepository repo) => repo.GetAll());
+app.MapGet("/houses", (IHouseRepository repo) => repo.GetAll())
+    .Produces<HouseDto[]>(StatusCodes.Status200OK);
+    
+app.MapGet("/house/{houseId:int}", async (int houseId, IHouseRepository repo) => {
+    var house = await repo.Get(houseId);
+    if (house == null) 
+        return Results.Problem($"House with ID {houseId} not found.", 
+            statusCode: 404);
+    return Results.Ok(house);
+}).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
 
 app.Run();
