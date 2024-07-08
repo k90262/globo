8309a6a33860e566649e1619ca907043fb92e698
Bill Ho <ycho.bill.lab@gmail.com>
2024-07-09 02:06:46 +0800

    [Adding Frontend Routing and Navigation] React: Setting up Routing
    npm install react-router-dom@6

--- ReactWeb/src/main/App.tsx
+++ ReactWeb/src/main/App.tsx
@@ -1,13 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HouseList from '../house/HouseList'
 import './App.css'
 import Header from './Header'
 
 function App() {
   return (
-   <div className="container">
-      <Header subtitle="Providing hourses all over the world" />
-      <HouseList/>
-   </div>
+    <BrowserRouter>
+       <div className="container">
+          <Header subtitle="Providing hourses all over the world" />
+          <Routes>
+            <Route path="/" element={<HouseList/>}></Route>
+          </Routes>
+      </div>
+    </BrowserRouter>
+
   )
 }
 
