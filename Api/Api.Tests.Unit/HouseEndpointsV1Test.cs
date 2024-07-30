e398becf2066f7b27a9688f7582ae78ac5799a7e
Bill Ho <ycho.bill.lab@gmail.com>
2024-07-30 23:59:20 +0800

    [Testing] added an unit test for endpoint GetAllHouses

--- Api/Api.Tests.Unit/HouseEndpointsV1Test.cs
+++ Api/Api.Tests.Unit/HouseEndpointsV1Test.cs
@@ -1,5 +1,6 @@
 using Api;
 using JetBrains.Annotations;
+using NSubstitute;
 
 namespace Api.Tests.Unit;
 
@@ -7,17 +8,29 @@ namespace Api.Tests.Unit;
 public class HouseEndpointsV1Test
 {
 
-    // Ref. https://github.com/dotnet/AspNetCore.Docs.Samples/blob/main/fundamentals/minimal-apis/samples/MinApiTestsSample/UnitTests/TodoMoqTests.cs
-    [Fact]
-    public void GetAllHouses_RepoHasData_ReturnsHourses()
-    {
-        //TODO
-    }
+    // Ref.
+    // About how to create the EndpontsV1Test from a minial api project: https://github.com/dotnet/AspNetCore.Docs.Samples/blob/main/fundamentals/minimal-apis/samples/MinApiTestsSample/UnitTests/TodoMoqTests.cs
+    // About how to use NSub: https://github.com/k90262/eShop/blob/main/tests/Basket.UnitTests/BasketServiceTests.cs
     
     [Fact]
-    public void GetAllHouses_RepoHasNoData_ReturnsEmptyList()
+    public async Task GetAllHouses_RepoHasData_ReturnsHourses()
     {
-        //TODO
+        // Arrange
+        var mockRepository = Substitute.For<IHouseRepository>();
+        List<HouseDto> items = [ new HouseDto(
+            Id: 1,
+            Address: "12 Valley of Kings, Geneva",
+            Country: "Switzerland",
+            Price: 900000)
+        ];
+        mockRepository.GetAll().Returns(Task.FromResult(items));
+        
+        // Act
+        var response = await HouseEndpointsV1.GetAllHouses(mockRepository);
+        
+        // Assert
+        await mockRepository.Received().GetAll();
+        Assert.Single(response);
     }
     
     [Fact]
